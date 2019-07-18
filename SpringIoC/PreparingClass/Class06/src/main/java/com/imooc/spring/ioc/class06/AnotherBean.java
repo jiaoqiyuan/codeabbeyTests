bffa51a36ff8062a761259927968bce011a21ffd
mac <mac@macdeMacBook-Pro.local>
2019-07-18 23:42:38 +0800

    add sout

--- SpringIoC/PreparingClass/Class06/src/main/java/com/imooc/spring/ioc/class06/AnotherBean.java
+++ SpringIoC/PreparingClass/Class06/src/main/java/com/imooc/spring/ioc/class06/AnotherBean.java
@@ -3,6 +3,7 @@ package com.imooc.spring.ioc.class06;
 public class AnotherBean {
 
     public AnotherBean() {
-        System.out.println(this.getClass().getSimpleName() + " has been created");
+        System.out.println(this.getClass().getSimpleName() +
+                " has been created");
     }
 }

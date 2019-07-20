ba0edae610924c879f0cd486b9f1474c0e94a6c5
jiaoqiyuan <jiaoqiyuan@outlook.com>
2019-07-20 23:16:49 +0800

    add spring

---
+++ src/main/java/Spring_Heima/Day1/src/main/java/JdbcDemo1.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class JdbcDemo1 {
+    public static void main(String[] args) throws SQLException {
+        //注册驱动
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        //获取连接
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/imooc_flink", "root", "1");
+        //获取数据库预处理对象
+        PreparedStatement statement = connection.prepareStatement("select * from student");
+        //执行sql
+        ResultSet resultSet = statement.executeQuery();
+        //遍历结果
+        while (resultSet.next()) {
+            System.out.println("name = " + resultSet.getString("name"));
+        }
+        //关闭
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}

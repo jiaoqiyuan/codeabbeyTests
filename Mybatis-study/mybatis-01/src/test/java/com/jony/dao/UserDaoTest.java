80c6065977a3b60eec3b02c4a0f3af54e99eef42
jiaoqiyuan <jiaoqiyuan@outlook.com>
2019-11-23 16:28:30 +0800

    update

--- Mybatis-study/mybatis-01/src/test/java/com/jony/dao/UserDaoTest.java
+++ Mybatis-study/mybatis-01/src/test/java/com/jony/dao/UserDaoTest.java
@@ -3,22 +3,41 @@ package com.jony.dao;
 import com.jony.pojo.User;
 import com.jony.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 
 public class UserDaoTest {
-    @Test
-    public void test() {
+    SqlSession sqlSession = null;
+    UserDao userDao = null;
+    @Before
+    public void before() {
         // 获取session对象
-        SqlSession sqlSession = MybatisUtils.getSqlSessionFactory();
+        sqlSession = MybatisUtils.getSqlSessionFactory();
         // getMapper
-        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        userDao = sqlSession.getMapper(UserDao.class);
+    }
+
+    @Test
+    public void testGetUserList() {
         // 调用接口
         List<User> userList = userDao.getUserList();
         for (User user : userList) {
             System.out.println(user);
         }
+    }
+
+    @Test
+    public void testGetUserById() {
+        //调用接口
+        User user = userDao.getUserById(1);
+        System.out.println(user.getName());
+    }
+
+    @After
+    public void after() {
         sqlSession.close();
     }
 }

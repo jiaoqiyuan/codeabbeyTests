9f3bc30fa1b5849094ab3a4d2d61e9ea47e83c88
jiaoqiyuan <jiaoqiyuan@outlook.com>
2019-11-25 00:30:51 +0800

    add mybatis04

--- Mybatis-study/mybatis-04/src/main/java/com/jony/dao/UserMapper.java
+++ Mybatis-study/mybatis-04/src/main/java/com/jony/dao/UserMapper.java
@@ -2,8 +2,17 @@ package com.jony.dao;
 
 import com.jony.pojo.User;
 
+import java.util.List;
+import java.util.Map;
+
 public interface UserMapper {
 
     // 根据 id 查询用户
     User getUserById(int id);
+
+    // 分页查询
+    List<User> getUserByLimit(Map<String, Object> map);
+
+    // RowBounds 分页，不推荐使用
+    List<User> getUserByRowBounds();
 }
--- Mybatis-study/mybatis-04/src/test/java/com/jony/dao/UserDaoTest.java
+++ Mybatis-study/mybatis-04/src/test/java/com/jony/dao/UserDaoTest.java
@@ -2,26 +2,65 @@ package com.jony.dao;
 
 import com.jony.pojo.User;
 import com.jony.utils.MybatisUtils;
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class UserDaoTest {
     public SqlSession sqlSession = null;
-    public UserMapper userDao = null;
+    public UserMapper userMapper = null;
+    public Logger logger = null;
     @Before
     public void before() {
         // 获取session对象
         sqlSession = MybatisUtils.getSqlSessionFactory();
         // getMapper
-        userDao = sqlSession.getMapper(UserMapper.class);
+        userMapper = sqlSession.getMapper(UserMapper.class);
+        logger = Logger.getLogger(UserDaoTest.class);
     }
 
     @Test
     public void testGetUserById() {
-        User userById = userDao.getUserById(3);
+        User userById = userMapper.getUserById(3);
         System.out.println(userById);
+    }
+
+    @Test
+    public void testLog4j() {
+        Logger logger = Logger.getLogger(UserDaoTest.class);
+        logger.info("Test log4j info!");
+        logger.debug("Test log4j debug!");
+        logger.error("Test log4j error!");
+    }
+
+    @Test
+    public void testGetUserByLimit() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("startIndex", 1);
+        map.put("pageSize", 2);
+        List<User> userByLimit = userMapper.getUserByLimit(map);
+        for (User user : userByLimit) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testGetUserByRowBounds() {
+        // 通过 Java 层面实现查询
+        RowBounds rowBounds = new RowBounds(1, 2);
+        List<User> list = sqlSession.selectList("com.jony.dao.UserMapper.getUserByRowBounds",
+                null, rowBounds);
+        for (User user : list) {
+            //System.out.println(user);
+            logger.info(user);
+        }
     }
 
     @After

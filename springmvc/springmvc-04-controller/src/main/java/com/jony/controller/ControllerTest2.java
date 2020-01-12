83a5d3124800c7764ab8fa25ab10852a00ecc3fc
jiaoqiyuan <jiaoqiyuan@outlook.com>
2020-01-12 18:06:06 +0800

    add 04 controller

---
+++ springmvc/springmvc-04-controller/src/main/java/com/jony/controller/ControllerTest2.java
@@ -0,0 +1,19 @@
+package com.jony.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 被注解的这个类中的所有方法，如果返回的是 String 类型的数据，
+ * 并且如果有具体的页面可以跳转，就会被视图解析器解析
+ */
+@Controller
+public class ControllerTest2 {
+
+    @RequestMapping("/test2")
+    public String test1(Model model) {
+        model.addAttribute("msg", "ControllerTest2.");
+        return "test1";
+    }
+}
